4d0073d43fc8e47a1e67cd9344bd54836a7ac43c
Mike Rottina <rottinam@gmail.com>
2025-05-17 10:34:57 -0400

    Remove hardcoded dropdown - implement localstorage last selected

--- src/boom.ts
+++ src/boom.ts
@@ -1,73 +1,115 @@
 
 const Boom = {
   init: () => {
+    let lastListenedTo = localStorage["lastListenedTo"] || "https://itunes.apple.com/us/rss/topsongs/limit=25/genre=18/explicit=true/json";
+    if (lastListenedTo) {
+      console.log("lastListenedTo: " + lastListenedTo);
+      localStorage["lastListenedTo"] = lastListenedTo;
+    } else {
+      console.log("lastListenedTo is null");
+    }
     Boom.populateSelector();
-    Boom.getTracks(
-      "https://itunes.apple.com/us/rss/topsongs/limit=25/genre=18/explicit=true/json"
-    );
+    Boom.getTracks(lastListenedTo);
   },
 
-  getTracks: async <T>(playlist: string): Promise<T> => {
-    const playlistInfo: string = "in gettracks " + playlist;
+  getTracks: async <T>(playlist: string): Promise<T | undefined> => {
     //console.log(playlist);
-    try {
-      const response = await fetch(playlist);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const publisherSlug = "&itscg=30200&itsct=music_box_link&ls=1&app=music&mttnsubad=1667990774&at=11l6841";
-      let parent = document.querySelector("ol");
-      if (parent) { parent.innerHTML = ""; } //clear children
-      const data = await response.json();
-      const tracks = data.feed.entry;
-      console.dir(tracks);
-      for(const track of tracks) {
-        let trackId = track.id.attributes["im:id"];
-        let trackArtist = track["im:artist"].label;
-        let trackTitle = track["im:name"].label; 
-        let trackImgUrl = track["im:image"][1].label;
-        let trackAudioUrl = track.link[1].attributes.href;
-        let trackAppleMusicUrl = track.id.label + publisherSlug;
-        let trackAlbumName = track["im:collection"]["im:name"].label;
 
-          // console.group(trackId);
-          console.log("trackAlbumName: " + trackAlbumName);
-          // console.log(
-          //   `%c${trackId}`,
-          //   "color:green",
-          //   trackArtist,
-          //   trackTitle
-          // );
-          // console.log("trackImgUrl: " + trackImgUrl);
-          // //console.log("trackAudioUrl: " + trackAudioUrl);
-          // console.log("trackAppleMusicUrl: " + trackAppleMusicUrl);
-          // console.groupEnd();
-          
-        Boom.displayTrack(trackId, trackArtist, trackTitle, trackImgUrl, trackAudioUrl, trackAppleMusicUrl, trackAlbumName);
-        //break; // debug: Remove this break to display all tracks
+    if (playlist.includes("pitchfork")) {
+      console.log("pitchfork playlist");
+
+      fetch(playlist)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+          const items = data.querySelectorAll("item");
+          items.forEach(item => {
+            const titleElement = item.querySelector("title");
+            const title = titleElement ? titleElement.textContent : null;
+            const creatorElement = item.querySelector("creator");
+            const creator = creatorElement ? creatorElement.textContent : null;
+            console.log("Title:", title);
+            console.log("Creator:", creator);
+            let appleMusicTerm = Boom.createItunesSearchTerm(creator + " " + title);
+          });
+        }).catch(error => console.error('Error fetching or parsing RSS:', error));
+      
+    } else {
+
+      try {
+        const response = await fetch(playlist);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const publisherSlug = "&itscg=30200&itsct=music_box_link&ls=1&app=music&mttnsubad=1667990774&at=11l6841";
+        let parent = document.querySelector("ol");
+        if (parent) { parent.innerHTML = ""; } //clear children
+        const data = await response.json();
+        const tracks = data.feed.entry;
+        console.dir(tracks);
+        for (const track of tracks) {
+          let trackId = track.id.attributes["im:id"];
+          let trackArtist = track["im:artist"].label;
+          let trackTitle = track["im:name"].label; 
+          let trackImgUrl = track["im:image"][1].label;
+          let trackAudioUrl = track.link[1].attributes.href;
+          let trackAppleMusicUrl = track.id.label + publisherSlug;
+          let trackAlbumName = track["im:collection"]["im:name"].label;
+
+            // console.group(trackId);
+            // console.log("trackAlbumName: " + trackAlbumName);
+            // console.log(
+            //   `%c${trackId}`,
+            //   "color:green",
+            //   trackArtist,
+            //   trackTitle
+            // );
+            // console.log("trackImgUrl: " + trackImgUrl);
+            // //console.log("trackAudioUrl: " + trackAudioUrl);
+            // console.log("trackAppleMusicUrl: " + trackAppleMusicUrl);
+            // console.groupEnd();
+            
+          Boom.displayTrack(trackId, trackArtist, trackTitle, trackImgUrl, trackAudioUrl, trackAppleMusicUrl, trackAlbumName);
+          //break; // debug: Remove this break to display all tracks
+        }
+        return data as T; // Return the data as the generic type T
       }
-      return data as T; // Return the data as the generic type T
+      catch (error: any) {
+        console.error("Fetch error:", error);
+        throw error;
+        return undefined; // Ensure a return value in case of an error
+      }
     }
-    catch (error: any) {
-      console.error("Fetch error:", error);
-      throw error;
-    }
+  },
+
+  createItunesSearchTerm: function createItunesSearchTerm(searchTerm: string) {
+    let searchUrl = "https://itunes.apple.com/search?limit=15&media=music&entity=song&country=us&term=";
+
+    let searchString = searchTerm.replace(/ /g, "+");
+    searchUrl += searchString;
+    console.log("createItunesSearchTermcreateItunesSearchTerm: " + searchUrl);
+
+    // createItunesSearchTermcreateItunesSearchTerm: https://itunes.apple.com/search?term=Eric+Torres+Echoes,+Spaces,+Lines
+
+    // fetch(searchUrl)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log("data: " + data);
+    //     console.dir(data);
+    //   })
+    //   .catch(error => console.error('Error fetching or parsing RSS:', error));
   },
 
   displayTrack: function displayTrack(trackId: string, trackArtist: string, trackTitle: string, trackImgUrl: string, trackAudioUrl: string, trackAppleMusicUrl: string, trackAlbumName: string) {
     //console.log("in displayTrack");
     let li = document.createElement("li");
     li.setAttribute("data-id", trackId);
-    // console.log("trackImgUrl: " + trackImgUrl);
-    // console.log("trackAudioUrl: " + trackAudioUrl);
-    // console.log("trackAppleMusicUrl: " + trackAppleMusicUrl);
     let Text = document.createTextNode(trackArtist);
     document.querySelector("ol")?.appendChild(li);
     Boom.generateAudioElement({li, trackArtist, trackAudioUrl, trackTitle, trackImgUrl, trackAppleMusicUrl, trackAlbumName});
   },
 
   generateAudioElement: function displayTrack({li, trackArtist, trackTitle, trackImgUrl, trackAudioUrl, trackAppleMusicUrl, trackAlbumName}:{li: HTMLLIElement, trackArtist: string, trackTitle: string,  trackImgUrl: string, trackAudioUrl: string, trackAppleMusicUrl: string, trackAlbumName: string}) {
-    //console.log("in generateAudioElement -------" + trackAudioUrl);
     let audio = document.createElement("audio");
     audio.setAttribute("type", "audio/mpeg");
     audio.setAttribute("src", "" + trackAudioUrl);
@@ -77,24 +119,23 @@ const Boom = {
 
     let link = document.createElement("a");
     link.setAttribute("href", trackAppleMusicUrl);
-    link.setAttribute("id", "x");
-    link.setAttribute("target", "_blank");
     link.setAttribute("rel", "noopener noreferrer");
+    link.setAttribute("target", "_blank");
     link.setAttribute("title", trackTitle);
 
     let amImage = document.createElement("img");
-    amImage.setAttribute("src", "../img/apple-music-compact.svg");
     amImage.setAttribute("alt", "Apple Music");
     amImage.setAttribute("class", "am");
+    amImage.setAttribute("src", "../img/apple-music-compact.svg");
     amImage.setAttribute("title", "Lsten on Apple Music");
 
     let img = document.createElement("img");
-    img.setAttribute("src", trackImgUrl);
+    img.setAttribute("alt", "album cover");
     img.setAttribute("class", "album");
+    img.setAttribute("src", trackImgUrl);
+    img.setAttribute("title", trackAlbumName);
     img.setAttribute("height", "60");
     img.setAttribute("width", "60");
-    img.setAttribute("alt", "album cover");
-    img.setAttribute("title", trackAlbumName);
 
     let h3 = document.createElement("h3");
     h3.setAttribute("class", "artist");
@@ -134,13 +175,14 @@ const Boom = {
           if (selector) {
               let optionElement = document.createElement("option");
               if (optionElement) {
-                optionElement.value = prefix + opt.value;
+                if (opt.type === "appmus") {
+                  optionElement.value = prefix + opt.value;
+                } else {
+                  optionElement.value = opt.value;
+                }
                 optionElement.textContent = opt.label;
                 if(opt.type === "na") {
                   optionElement.setAttribute("disabled", "disabled");
-                }
-                if(opt.type === "custom") {
-                  // implement apple music lookup
                 }
               }
               if (optionElement) {
@@ -149,6 +191,8 @@ const Boom = {
               selector.addEventListener("change", (event) => {
               let selectedValue = (event.target as HTMLSelectElement).value;
               console.log("Selected value:", selectedValue);
+              localStorage["lastListenedTo"] = selectedValue;
+              console.log("localStorage['lastListenedTo']: " + localStorage["lastListenedTo"]);
               // Call getTracks with the selected value
               Boom.getTracks(selectedValue);
             });
