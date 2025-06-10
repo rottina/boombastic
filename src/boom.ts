9aa1dced9e6d505eb0b80f41965e1c285ab8b891
Mike Rottina <rottinam@gmail.com>
2025-06-09 22:40:14 -0400

    More optimizations!

--- src/boom.ts
+++ src/boom.ts
@@ -1,18 +1,17 @@
+const defaultPlaylist =
+  "https://itunes.apple.com/us/rss/topsongs/limit=25/genre=18/explicit=true/json";
+const localStorageKey = "lastListenedTo";
+const localStorageVal =
+  localStorage.getItem("lastListenedTo") || defaultPlaylist;
 const publisherSlug =
   "&itscg=30200&itsct=music_box_link&ls=1&app=music&mttnsubad=1667990774&at=11l6841";
 const githubPrefix =
   "https://raw.githubusercontent.com/rottina/boombastic/refs/heads/main/src/playlists/";
 const itunesApiPrefix =
   "https://itunes.apple.com/search?limit=1&media=music&entity=song&term=";
-const defaultPlaylist =
-  "https://itunes.apple.com/us/rss/topsongs/limit=25/genre=18/explicit=true/json";
-const localStorageKey = "lastListenedTo";
-const localStorageVal =
-  localStorage.getItem("lastListenedTo") || defaultPlaylist;
 const appleMusicPrefix =
   "https://itunes.apple.com/us/rss/topsongs/limit=25/genre=";
 const appleMusicSuffix = "/explicit=true/json";
-
 const context = new AudioContext();
 const analyser = context.createAnalyser();
 const audios = undefined;
@@ -23,20 +22,10 @@ let sourceNode = undefined;
 const visualisation = document.getElementById("viz");
 
 class Boomer {
-  // OK boomer!
+  // OK Boomer!
   constructor() {
-    this.getTracks(defaultPlaylist);
-  }
-
-  getTracks(playlist: string) {
-    console.log(playlist);
-    if (playlist.includes("pitchfork")) {
-      console.log("pitchfork");
-    } else if (playlist.includes("custom")) {
-      console.log("custom");
-    } else {
-      console.log("default");
-    }
+    Playlist.prototype.getPlaylist(defaultPlaylist);
+    console.log("Boomer initialized with default playlist:", defaultPlaylist);
   }
 }
 
@@ -59,7 +48,6 @@ class Header extends HTMLElement {
   }
 
   populateSelector(appleMusicPrefix: string, appleMusicSuffix: string) {
-    console.log("in populateSelect");
     const optionsFile = "options.json";
     fetch(optionsFile)
       .then((response) => response.json())
@@ -82,6 +70,15 @@ class Header extends HTMLElement {
           }
           selector.appendChild(optionElement);
         }
+      })
+      .then(() => {
+        this.dispatchEvent(
+          new CustomEvent("playlist-changed", {
+            detail: { selectedValue: this.querySelector("select")?.value },
+            bubbles: true,
+            composed: true,
+          }),
+        );
       });
   }
 
@@ -141,14 +138,14 @@ class Playlist extends HTMLElement {
           const parts = finalSearchString.split("/");
           finalSearchString = parts.pop() || "";
           finalSearchString = finalSearchString.replace(/-/g, " ");
-          Boom.itunesSearch(finalSearchString);
+          Synth.itunesSearch(finalSearchString);
         }
       } else if (playlist.includes("custom")) {
         const tracks = data;
         for (const track of tracks) {
           const searchTerm = `${track.s} ${track.a}`;
           const finalSearchString = searchTerm.replace(/ /g, "+");
-          Boom.itunesSearch(finalSearchString);
+          Synth.itunesSearch(finalSearchString);
         }
       } else {
         const tracks = data.feed.entry;
@@ -160,7 +157,7 @@ class Playlist extends HTMLElement {
           const trackAudioUrl = track.link[1].attributes.href;
           const trackAppleMusicUrl = track.id.label + publisherSlug;
           const trackAlbumName = track["im:collection"]["im:name"].label;
-          Boom.displayTrack(
+          Synth.displayTrack(
             trackId,
             trackArtist,
             trackTitle,
@@ -180,10 +177,10 @@ class Playlist extends HTMLElement {
 
   connectedCallback() {
     console.log("playlist component added to DOM");
-    const Header = document.querySelector("header-component");
-    if (Header) {
+    const headerComponent = document.querySelector("header-component");
+    if (headerComponent) {
       console.log("header component found");
-      Header.addEventListener("playlist-changed", (event) => {
+      document.addEventListener("playlist-changed", (event) => {
         const selectedValue = (event as CustomEvent).detail.selectedValue;
         localStorage.setItem("lastListenedTo", selectedValue);
         console.log("Selected value from playlist:", selectedValue);
@@ -197,24 +194,59 @@ class Playlist extends HTMLElement {
 }
 customElements.define("playlist-component", Playlist);
 
-const Boom = {
-  init: () => {
-    const lastListenedTo = localStorage.lastListenedTo || defaultPlaylist;
-    if (lastListenedTo) {
-      console.log(`lastListenedTo: ${lastListenedTo}`);
-      localStorage.lastListenedTo = lastListenedTo;
-    } else {
-      console.log("lastListenedTo is not set, using default playlist");
-      localStorage.lastListenedTo = defaultPlaylist;
-    }
-    Boom.populateSelector(appleMusicPrefix, appleMusicSuffix);
-    Boom.getTracks(lastListenedTo);
-  },
+class TrackPanel extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.createElement("template");
+    template.innerHTML = `
+    <li> </li>
+    `;
+    this.appendChild(template.content.cloneNode(true));
+  }
 
-  getTracks: async <T>(playlist: string): Promise<T | undefined> => {
-    return undefined;
-  },
+  static get observedAttributes() {
+    return [
+      "track-artist",
+      "track-title",
+      "track-img-url",
+      "track-audio-url",
+      "track-apple-music-url",
+      "track-album-name",
+    ];
+  }
 
+  connectedCallback(): void {
+    console.log("track-panel component added to DOM");
+    this.setAttribute("track-artist", "Unknown Artist");
+    this.setAttribute("track-title", "Unknown Title");
+    this.setAttribute("track-img-url", "https://via.placeholder.com/60");
+    this.setAttribute("track-audio-url", "https://example.com/audio.mp3");
+    this.setAttribute(
+      "track-apple-music-url",
+      "https://example.com/apple-music",
+    );
+    this.setAttribute("track-album-name", "Unknown Album");
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    const li = this.querySelector("li")!;
+    li.setAttribute("data-id", "track-id-placeholder");
+    const trackArtist = this.getAttribute("track-artist") || "Unknown Artist";
+    const trackTitle = this.getAttribute("track-title") || "Unknown Title";
+    const trackImgUrl =
+      this.getAttribute("track-img-url") || "https://via.placeholder.com/60";
+    const trackAudioUrl =
+      this.getAttribute("track-audio-url") || "https://example.com/audio.mp3";
+    const trackAppleMusicUrl =
+      this.getAttribute("track-apple-music-url") ||
+      "https://example.com/apple-music";
+    const trackAlbumName =
+      this.getAttribute("track-album-name") || "Unknown Album";
+
+    //generateTrackPanel: function generateTrackPane do this here
+  }
+}
+customElements.define("track-panel", TrackPanel);
+
+const Synth = {
   itunesSearch: (finalSearchString: string) => {
     const searchUrl = itunesApiPrefix + finalSearchString;
 
@@ -230,7 +262,7 @@ const Boom = {
           const trackAppleMusicUrl =
             res.results[0].collectionViewUrl + publisherSlug;
           const trackAlbumName = res.results[0].collectionName;
-          Boom.displayTrack(
+          Synth.displayTrack(
             trackId,
             trackArtist,
             trackTitle,
@@ -261,7 +293,7 @@ const Boom = {
     const li = document.createElement("li");
     li.setAttribute("data-id", trackId);
     document.querySelector("ul")?.appendChild(li);
-    Boom.generateTrackPanel({
+    Synth.generateTrackPanel({
       li,
       trackArtist,
       trackAudioUrl,
@@ -334,6 +366,7 @@ const Boom = {
     link.appendChild(img);
     link.appendChild(h3);
     link.appendChild(h4);
+    console.log(li);
     li.appendChild(link);
     li.appendChild(audio);
     //console.trace();
@@ -341,56 +374,8 @@ const Boom = {
     document.querySelector("ul")?.appendChild(li);
   },
 
-  populateSelector: async <T>(
-    appleMusicPrefix: string,
-    appleMusicSuffix: string,
-  ): Promise<T> => {
-    console.log("in populateSelect");
-    const optionsFile = "options.json";
-    try {
-      const response = await fetch(optionsFile);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      const opts = data.options;
-      // biome-ignore lint/style/noNonNullAssertion: <explanation>
-      const selector = document.querySelector("select")!;
-      for (const opt of opts) {
-        const optionElement = document.createElement("option");
-        if (opt.type === "appmus") {
-          optionElement.value = appleMusicPrefix + opt.value + appleMusicSuffix;
-        } else {
-          optionElement.value = opt.value;
-        }
-        optionElement.textContent = opt.label;
-        if (opt.type === "na") {
-          optionElement.setAttribute("disabled", "disabled");
-        }
-        if (opts.value === localStorage.lastListenedTo) {
-          console.log(
-            `localStorage.lastListenedTo: ${localStorage.lastListenedTo}`,
-          );
-          optionElement.setAttribute("selected", "selected");
-        }
-        selector.appendChild(optionElement);
-      }
-      selector.addEventListener("change", (event) => {
-        const selectedValue = (event.target as HTMLSelectElement).value;
-        console.log("Selected value:", selectedValue);
-        localStorage.setItem("lastListenedTo", selectedValue);
-        console.log(`changed lastListenedTo: ${selectedValue}`);
-        Boom.getTracks(selectedValue);
-      });
-      return data as T; // Return the data as the generic type T
-    } catch (error: unknown) {
-      console.error("Fetch error:", error);
-      throw error;
-    }
-  },
-
   update: () => {
-    requestAnimationFrame(Boom.update);
+    requestAnimationFrame(Synth.update);
     analyser.getByteFrequencyData(frequencyData);
     Array.from(document.querySelectorAll("#viz > div")).forEach(
       (bar, index) => {
@@ -416,28 +401,29 @@ const Boom = {
       div.style.left = `${i * barSpacingPercent}%`;
       visualisation?.appendChild(div);
     }
-    Boom.update();
+    Synth.update();
   },
 }; // Closing brace for Boom object
+
+const boomer = new Boomer(); //Esiason or Berman
 
 document.addEventListener(
   "play",
   (ag) => {
     const audios = document.getElementsByTagName("audio");
     for (const audio of audios) {
-      //Boom.setCurrentAudio(audio);
+      //Synth.setCurrentAudio(audio);
       if (audio !== ag.target) {
         audio.pause();
       } else {
-        Boom.setCurrentAudio(audio as HTMLAudioElement);
-        //Boom.setCurrentAudio(audio);
+        Synth.setCurrentAudio(audio as HTMLAudioElement);
+        //Synth.setCurrentAudio(audio); ?
       }
     }
   },
   true,
 );
 
-const boomer = new Boomer();
 window.addEventListener("load", () => {
   console.log("boom! lets go...");
 });
